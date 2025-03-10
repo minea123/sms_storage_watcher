01190c3013c7ec1d21a656bf13bb31e978f5f578
minea123 <chanminea.houn@mjqeducation.edu.kh>
2025-03-10 13:38:11 +0700

    update ignore dir pattern

--- watch_sms_storage.sh
+++ watch_sms_storage.sh
@@ -1,6 +1,6 @@
 TARGET=/App/aii_school_prod/storage/app
 
-inotifywait -m -r "$TARGET" -e create -e modify --exclude '^\.' --exclude '\.tmp$|\.swp$' 2>/dev/null | while read -r path event file; do
+inotifywait -m -r "$TARGET" -e create -e modify --exclude '\.tmp$|\.swp$' --exclude '(^|/)[._]' 2>/dev/null | while read -r path event file; do
     # is event is file, not dir
     if [[ "$event" != *"ISDIR"* ]]; then
         file_path="$path$file"
