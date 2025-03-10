c9a97a2fc617aa0f5dbeaa1af2e9a1dc4727d1be
minea123 <chanminea.houn@mjqeducation.edu.kh>
2025-03-10 12:47:22 +0700

    kill previous process before start background process

---
+++ run_background.sh
@@ -0,0 +1,3 @@
+cd $PWD
+echo $PWD
+sudo nohup ./watch.sh >> app.log 2>> app.err.log &
--- watch.sh
+++ watch.sh
@@ -1,6 +1,12 @@
+if [ -s pid.txt ]; then
+    sudo kill -9 $(cat pid.txt)
+fi
+
+echo $! > pid.txt
+
 TARGET=/App/aii_school_prod/storage/app
 
-inotifywait -m -r "$TARGET" -e create -e modify 2>/dev/null | while read -r path event file; do
+inotifywait -m --exclude '^\.' -r "$TARGET" -e create -e modify 2>/dev/null | while read -r path event file; do
     # is event is file, not dir
     if [[ "$event" != *"ISDIR"* ]]; then
         file_path="$path$file"
