1e0ae5ab2e1ffcd5f89fb78e1a9657dfe8c3c40f
minea123 <chanminea.houn@mjqeducation.edu.kh>
2025-03-11 10:06:04 +0700

    remove verbose mode

--- transfer.sh
+++ transfer.sh
@@ -14,5 +14,5 @@ for server in $(cat $SERVER_LIST); do
 
 
     # scp to other dir
-    rsync -avvv --log-file=transfer.log --ignore-existing $FILE_PATH $SSH_SERVER:$FILE_PATH
+    rsync -av --ignore-existing $FILE_PATH $SSH_SERVER:$FILE_PATH
 done
