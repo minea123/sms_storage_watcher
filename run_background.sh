4db8578fb50998b7ec9d7d9010b6ac28f1f4ef0c
minea123 <chanminea.houn@mjqeducation.edu.kh>
2025-03-11 09:12:46 +0700

    run initialize syncronize before start sync in realtime

--- run_background.sh
+++ run_background.sh
@@ -1,3 +1,15 @@
 cd $PWD
 sudo ./kill.sh
+SERVER_LIST="servers.list"
+
+echo "start initial syncronize..."
+
+for server in $(cat $SERVER_LIST); do
+    echo "resync from server $server"
+    SSH_SERVER=root@$server
+
+    # scp to other dir
+    sudo rsync -avz --progress --ignore-existing $SSH_SERVER:/App/aii_school_prod/storage/app /App/aii_school_prod/storage
+done
+
 sudo nohup /bin/bash ./watch_sms_storage.sh >> app.log 2>> app.err.log &
