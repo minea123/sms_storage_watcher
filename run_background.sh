8217a3f6ce365727a8ec2d6155ae5da0d00cb26e
minea123 <chanminea.houn@mjqeducation.edu.kh>
2025-03-11 09:19:52 +0700

    initial sync with ignore mpdf temp dir

--- run_background.sh
+++ run_background.sh
@@ -9,7 +9,7 @@ for server in $(cat $SERVER_LIST); do
     SSH_SERVER=root@$server
 
     # scp to other dir
-    sudo rsync -avz --progress --ignore-existing $SSH_SERVER:/App/aii_school_prod/storage/app /App/aii_school_prod/storage
+    sudo rsync -avz --progress --ignore-existing --exclude 'mpdf' $SSH_SERVER:/App/aii_school_prod/storage/app /App/aii_school_prod/storage
 done
 
 sudo nohup /bin/bash ./watch_sms_storage.sh >> app.log 2>> app.err.log &
