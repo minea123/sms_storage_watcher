bb3a167b06342b7402d716298c2e9661c43b74fb
minea123 <chanminea.houn@mjqeducation.edu.kh>
2025-03-12 13:22:04 +0700

    update script run background

--- run_background.sh
+++ run_background.sh
@@ -2,4 +2,5 @@ cd $PWD
 sudo ./kill.sh
 sudo ./resync.sh
 
-sudo nohup /bin/bash ./watch_sms_storage.sh >> app.log 2>> app.err.log &
+sudo /bin/bash -c "nohup ./watch_sms_upload.sh >> app.log 2>> app.err.log &"
+sudo /bin/bash -c "nohup ./watch_sms_public.sh >> app.log 2>> app.err.log &"
