5bda03990d87bf22c4510ba4ff8c99df747170d6
minea123 <chanminea.houn@mjqeducation.edu.kh>
2025-03-12 15:37:59 +0700

    update watch list

--- watch_sms_storage.sh
+++ watch_sms_storage.sh
@@ -1,5 +1,5 @@
+sudo rm watch.tmp
 WATCH_PATH=/App/aii_school_prod/storage/app
-
 CURRENT_DATE=$(date +"%Y/%m/%d")
 
 while IFS= read -r path; do
@@ -13,13 +13,15 @@ while IFS= read -r path; do
         sudo mkdir -p $path
     fi
     # Add your action here
+
+    echo $path >> watch.tmp
 done < "./base_dir"
 
 sudo chown www-data:www-data -R /App/aii_school_prod/storage/app
 sudo chmod 777 -R /App/aii_school_prod/storage/app
 
-find /App/aii_school_prod/storage/app/ -type d | grep "$CURRENT_DATE\$" | while read -r path; do
-    WATCH_DIR="$path"
+while IFS= read -r path; do
+    # Skip empty lines
     echo "Watching $WATCH_DIR"
     sudo nohup /bin/bash ./watch_dir_event.sh $path &
-done
+done < "./watch.tmp"
